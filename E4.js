4d9cf02994f9d4ce5216d4f3ab6f3287f1c55c55
jhael07 <jhaelr007@gmail.com>
2023-02-28 01:11:40 -0400

    cuarto refactorizado

--- E4.js
+++ E4.js
@@ -7,32 +7,30 @@ Mostrar en la web dicha tabla con una buena apariencia, usando un ModalBox.
 
 */
 
-const btnEnviar = document.querySelector("#btn-enviar");
-const valor = document.querySelector("#valor");
-const tabla = document.querySelector("#tabla");
-
-// verificar si ya habia valores
+const btnEnviar = document.querySelector("#btn-enviar"),
+	valor = document.querySelector("#valor"),
+	tabla = document.querySelector("#tabla");
 
 btnEnviar.addEventListener("click", () => {
+	//eliminar datos anteriores
 	tabla.innerHTML = "";
+
 	// Guarda el valor del input
 	const valorOriginal = +valor.value;
-	const arr = [];
 
-	//eliminar datos anteriores
-
+	if (valorOriginal < 1 || valorOriginal > 10) {
+		alert("El valor debe estar entre 1 y 10");
+		return;
+	}
 	// hace la tabla de multiplicacion
 	for (let i = 0; i < 12; i++) {
 		const numeroParaMultiplicar = i + 1;
 		const resultado = valorOriginal * numeroParaMultiplicar;
-		arr.push(
-			`<tr ><td>${valorOriginal} x ${numeroParaMultiplicar}
-     </td><td> ${resultado}</td></tr>  `
-		);
-	}
+		const html = `<tr>
+                  <td>${valorOriginal} x ${numeroParaMultiplicar}</td>
+                  <td> ${resultado}</td>
+                  </tr>`;
 
-	// Inserta los datos en un elemento html
-	for (let i = 0; i < arr.length; i++) {
-		tabla.insertAdjacentHTML("beforeend", arr[i]);
+		tabla.insertAdjacentHTML("beforeend", html);
 	}
 });
